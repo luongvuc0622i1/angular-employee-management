66ac660f0b77861819bdd96d35327b621ff0fb1b
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-10-23 14:24:39 +0700

    a

--- src/app/profile-employee/profile-employee.component.ts
+++ src/app/profile-employee/profile-employee.component.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
-import { EmployeeService } from '../service/employee.service';
 import { TokenService } from '../service/token.service';
+import { Component, OnInit, Inject } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { EmployeeService } from '../service/employee.service';
+import { EditEmployeeComponent } from '../edit-employee/edit-employee.component';
 
 @Component({
   selector: 'app-profile-employee',
   templateUrl: './profile-employee.component.html',
   styleUrls: ['./profile-employee.component.css']
 })
-export class ProfileEmployeeComponent {
+export class ProfileEmployeeComponent implements OnInit {
   // @ts-ignore
-  employee: Employee;
+  employeeForm: FormGroup;
+  id: number;
 
-  constructor(private employeeService: EmployeeService, private tokenService: TokenService) {
+  constructor(private employeeService: EmployeeService,
+              private tokenService: TokenService,
+              public dialogRef: MatDialogRef<EditEmployeeComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.id = this.data.id;
+    this.getEmployee();
   }
 
   ngOnInit() {
-    this.getById();
   }
 
-  getById() {
-    this.employeeService.findById(parseInt(this.tokenService.getID())).subscribe(employee => {
-      this.employee = employee;
-      console.log(employee)
+  getEmployee() {
+    return this.employeeService.findById(parseInt(this.tokenService.getID())).subscribe(employee => {
+      this.employeeForm = new FormGroup({
+        username: new FormControl(employee.username),
+        password: new FormControl(employee.password),
+        firstName: new FormControl(employee.firstName),
+        lastName: new FormControl(employee.lastName),
+        email: new FormControl(employee.email),
+        phone: new FormControl(employee.phone),
+        status: new FormControl(employee.status),
+      });
     });
   }
 }
